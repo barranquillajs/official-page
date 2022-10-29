e8488c852929dce7d1f31904383fb6d43e5832ff
Jesus Bossa <x@jesusbossa.dev>
2022-10-29 12:44:18 -0500

    feat: add style system

---
+++ components/header/header.tsx
@@ -0,0 +1,39 @@
+import cn from 'classnames';
+import Image from 'next/image';
+import { FaTwitterSquare, FaLinkedin, FaWhatsapp, FaMeetup } from 'react-icons/fa';
+
+import { FluidContainer, TopBar } from '@makinox/makinox-ui';
+
+import headerimage from '../../public/assets/BAQJS.png';
+
+export default function Navbar() {
+  const classes = {
+    item: cn('ml-1 text-primary-400 text-xl'),
+  };
+
+  return (
+    <header className={`${TopBar({ isDark: false })}`}>
+      <nav className={`flex justify-between items-center py-2 ${FluidContainer()} `}>
+        <div className="flex items-center">
+          <Image className="rounded-md" src={headerimage} alt="BarranquillaJS logo" width={30} height={30} />
+          <span className="ml-2">Barranquilla JS</span>
+        </div>
+
+        <div className="flex">
+          <a className={classes.item} href="https://www.meetup.com/es/barranquillajs" target="_blank" rel="noreferrer">
+            <FaMeetup />
+          </a>
+          <a className={classes.item} href="https://chat.whatsapp.com/Cgkiea91hbpH2kErkjrmxb" target="_blank" rel="noreferrer">
+            <FaWhatsapp />
+          </a>
+          <a className={classes.item} href="https://twitter.com/baq_js" target="_blank" rel="noreferrer">
+            <FaTwitterSquare />
+          </a>
+          <a className={classes.item} href="https://www.linkedin.com/groups/12717860/" target="_blank" rel="noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      primary: {
+        50: '#fdfde9',
+        100: '#fafbc6',
+        200: '#f9f68f',
+        300: '#f5e94f',
+        400: '#efd81b',
+        500: '#dfc113',
+        600: '#c1980d',
+        700: '#9a6e0e',
+        800: '#7f5714',
+        900: '#6d4716',
+      },
+      secondary: {
+        50: '#f2f7fd',
+        100: '#e5edf9',
+        200: '#c5d9f2',
+        300: '#91b9e8',
+        400: '#5695da',
+        500: '#2f74c0',
+        600: '#215da8',
+        700: '#1c4b88',
+        800: '#1b4171',
+        900: '#1b375f',
+      },
+      base: {
+        white: '#FFFFFF',
+        black: '#000000',
+      },
+      transparent: 'transparent',
+    },
+  },
+  plugins: [],
+};
