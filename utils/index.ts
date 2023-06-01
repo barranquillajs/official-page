90b1f7ea26dcffd4c6ad8ca700df1f4cbd9a8fd1
Jesus Bossa <x@jesusbossa.dev>
2023-06-01 10:22:40 -0500

    feat: improve open graph values

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
 export { client } from './apollo';
 export * from './metadata';
 export * from './types';
+
+export const getItIsProductionEnv = () => process.env.NODE_ENV === 'production';
--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,13 +1,11 @@
 import { Metadata } from 'next';
-import { StaticImageData } from 'next/image';
-
-import baqjsLogo from '../public/assets/BAQJS.png';
+import { getItIsProductionEnv } from '.';
 
 type HelmetProps = {
   title?: string;
   description?: string;
   keywords?: string;
-  image?: StaticImageData;
+  image?: string;
   url?: string;
 };
 
@@ -15,23 +13,53 @@ export const returnMetadata = ({
   title = 'Barranquilla JS - Pagina Oficial',
   description = 'Este es un grupo para todos los desarrolladores, entusiastas y apasionados por Javascript en Barranquilla y la costa.',
   keywords = 'barranquilla, js, javascript, meetup, comunidad, desarrollo, programacion, tecnologia, costa, atlantico, colombia',
-  image = baqjsLogo,
+  image,
 }: HelmetProps): Metadata => {
+  const isProductionEnv = getItIsProductionEnv();
+  const metadataBase = isProductionEnv ? new URL(process.env.URL) : new URL(`http://localhost:${process.env.PORT || 3000}`);
+  const usableImage = image || '/assets/BAQJS.png';
+
   return {
     title,
     description,
     keywords,
     applicationName: 'Barranquilla JS',
+    metadataBase,
     icons: {
-      icon: '/favicon.ico',
+      icon: '/assets/BAQJS.png',
+      shortcut: usableImage,
+      apple: usableImage,
+      other: {
+        rel: 'apple-touch-icon-precomposed',
+        url: usableImage,
+      },
     },
     openGraph: {
       title,
       description,
-      images: {
-        url: image.src,
+      type: 'website',
+      locale: 'es',
+      images: [
+        {
+          url: usableImage,
+          width: 800,
+          height: 600,
+        },
+        {
+          url: usableImage,
+          width: 1200,
+          height: 300,
+        },
+      ],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: true,
       },
-      locale: 'es_CO',
     },
   };
 };
