8b5dd189451104bb6b30ec4fcf35f0001b4a6a8c
Jesus Hernandez <jesusbeckan@gmail.com>
2024-02-27 19:45:55 -0500

    fix: updates canonical

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,5 +8,6 @@ export const LINK_BARRANQUILLA_TIKTOK = 'https://www.tiktok.com/@barranquillajs'
 export const LINK_BARRANQUILLA_TWITCH = 'https://www.twitch.tv/barranquillajs';
 export const LINK_BARRANQUILLA_YOUTUBE = 'https://www.youtube.com/channel/UC5VPQwD2C6RlLbifNapzhJA';
 export const DEFAULT_PHOTO_URL = "https://picsum.photos/id/25/372/250"
+export const DOMAIN = "https://barranquillajs.org"
 
 export * from './pricing';
