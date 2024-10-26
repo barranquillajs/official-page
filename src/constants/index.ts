7288ab73a8fad49973b8fb0b3000940c1acaa634
Gabriel Landinez <gabol@MacBook-Air.local>
2024-10-26 11:01:27 -0500

    add github icon

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,6 +6,7 @@ export const LINK_BARRANQUILLA_TWITTER = 'https://twitter.com/baq_js';
 export const LINK_BARRANQUILLA_INSTAGRAM = 'https://www.instagram.com/barranquilla.js/';
 export const LINK_BARRANQUILLA_TIKTOK = 'https://www.tiktok.com/@barranquillajs';
 export const LINK_BARRANQUILLA_TWITCH = 'https://www.twitch.tv/barranquillajs';
+export const LINK_BARRANQUILLA_GITHUB = 'https://github.com/barranquillajs';
 export const LINK_BARRANQUILLA_YOUTUBE = 'https://www.youtube.com/channel/UC5VPQwD2C6RlLbifNapzhJA';
 export const DEFAULT_PHOTO_URL = "https://picsum.photos/id/25/372/250"
 export const DOMAIN = "https://barranquillajs.org"
