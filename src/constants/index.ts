ce20e5d66d9a0c59bd118f365c8883375682ec9c
Isaac Navarro <isaacnavarro2000@gmail.com>
2024-10-27 23:19:21 -0500

    feat: se agregó el ícono de github para redireccionar al perfil de barranquillajs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export const LINK_BARRANQUILLA_MEETUP = 'https://www.meetup.com/es/barranquillaj
 export const LINK_BARRANQUILLA_LINKEDIN = 'https://www.linkedin.com/company/89602992';
 export const LINK_BARRANQUILLA_DISCORD = 'https://bit.ly/3Nb3Eon';
 export const LINK_BARRANQUILLA_WHATSAPP = 'https://chat.whatsapp.com/Cgkiea91hbpH2kErkjrmxb';
+export const LINK_BARRANQUILLA_GITHUB = 'https://github.com/barranquillajs';
 export const LINK_BARRANQUILLA_TWITTER = 'https://twitter.com/baq_js';
 export const LINK_BARRANQUILLA_INSTAGRAM = 'https://www.instagram.com/barranquilla.js/';
 export const LINK_BARRANQUILLA_TIKTOK = 'https://www.tiktok.com/@barranquillajs';
